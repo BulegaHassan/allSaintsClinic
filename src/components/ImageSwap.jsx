6203ac20fa6ee733a1f50224960eb90d17924122
Hassan Bulega <hassanbulega@gmail.com>
2024-02-03 14:24:38 +0300

    swap images

--- src/components/ImageSwap.jsx
+++ src/components/ImageSwap.jsx
@@ -10,7 +10,7 @@ const ImageSwap = ({ imgURL, changeBigMedImage, bigMedImg }) => {
     <div className={`flex justify-center cursor-pointer sm:m-8 md:m-0 max-sm:flex-1`} onClick={handleClick}>      
         <img
           src={imgURL.thumbnail}
-          alt='med image colletion'
+          alt='medical image colletion'
           width={100}
           height={103.34}
           className='rounded-sm'
