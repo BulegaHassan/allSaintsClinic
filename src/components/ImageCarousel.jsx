013f4e08ac5c26859130c4c8934bf8c3b37df057
Hassan Bulega <hassanbulega@gmail.com>
2024-02-03 14:23:19 +0300

    feat: add images

--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -1,21 +1,39 @@
+import baby from "../assets/baby.jpg";
+import babyThumbnail from "../assets/baby-thumbnail.jpg";
+import consultation from "../assets/consultation.jpg";
+import consultationThumbnail from "../assets/consultation-thumbnail.jpg";
+import microscope from "../assets/microscope.jpg";
+import microscopeThumbnail from "../assets/microscope-thumbnail.jpg";
+import xray from "../assets/xray.jpg";
+import xrayThumbnail from "../assets/xray-thumbnail.jpg";
+import paed from "../assets/paed.jpg";
+import paedThumbnail from "../assets/paed-thumbnail.jpg";
 import ImageGallery from "react-image-gallery";
-import "react-image-gallery/styles/css/image-gallery.css"
+import "react-image-gallery/styles/css/image-gallery.css";
 const images = [
   {
-    original: "https://picsum.photos/id/1018/1000/600/",
-    thumbnail: "https://picsum.photos/id/1018/250/150/",
+    original: consultation,
+    thumbnail: consultationThumbnail,
   },
   {
-    original: "https://picsum.photos/id/1015/1000/600/",
-    thumbnail: "https://picsum.photos/id/1015/250/150/",
+    original: microscope,
+    thumbnail: microscopeThumbnail,
   },
   {
-    original: "https://picsum.photos/id/1019/1000/600/",
-    thumbnail: "https://picsum.photos/id/1019/250/150/",
+    original: paed,
+    thumbnail: paedThumbnail,
+  },
+  {
+    original: xray,
+    thumbnail: xrayThumbnail,
+  },
+  {
+    original: baby,
+    thumbnail: babyThumbnail,
   },
 ];
 
 const ImageCarousel = () => {
-  return <ImageGallery items={images} original thumbnail/>;
-}
-export default ImageCarousel
+  return <ImageGallery items={images} original thumbnail />;
+};
+export default ImageCarousel;
